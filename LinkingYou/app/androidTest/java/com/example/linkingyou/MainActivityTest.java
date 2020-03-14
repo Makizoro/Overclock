f3744d54483dd4b8d676dacc266bcaa84e548aaa
Makizoro <39122438+Makizoro@users.noreply.github.com>
2020-03-14 02:30:19 +0000

    Add files via upload

---
+++ LinkingYou/app/androidTest/java/com/example/linkingyou/MainActivityTest.java
@@ -0,0 +1,45 @@
+package com.example.linkingyou;
+
+import androidx.test.rule.ActivityTestRule;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static org.junit.Assert.*;
+
+public class MainActivityTest {
+    @Rule
+    public
+    ActivityTestRule<MainActivity> registerActivityActivityTestRule = new ActivityTestRule<MainActivity>(MainActivity.class);
+
+    // To test the person number editText
+    @Test
+    public void usernameText() {
+        onView(withId(R.id.editText)).perform(typeText("123456"));
+    }
+
+    // To test the password
+    @Test
+    public void passText(){
+        onView(withId(R.id.edPass)).perform(typeText("hey"));
+    }
+
+    // To test the link to the register page
+    @Test
+    public void regLink(){
+        onView(withId(R.id.txtReg)).perform(click());
+    }
+
+    // To test the login
+    @Test
+    public void login(){
+        onView(withId(R.id.editText)).perform(typeText("123456"));
+        onView(withId(R.id.edPass)).perform(typeText("hey"));
+        onView(withId(R.id.btnLog)).perform(click());
+
+    }
+}
