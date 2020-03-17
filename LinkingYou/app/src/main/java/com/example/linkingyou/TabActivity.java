a54afc520d430cf5bbe7f114b46f2745ae7965ba
Makizoro <39122438+Makizoro@users.noreply.github.com>
2020-03-17 23:21:00 +0000

    Add files via upload

---
+++ LinkingYou/app/src/main/java/com/example/linkingyou/TabActivity.java
@@ -0,0 +1,43 @@
+package com.example.linkingyou;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.tabs.TabLayout;
+
+import androidx.viewpager.widget.ViewPager;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.example.linkingyou.ui.main.SectionsPagerAdapter;
+
+public class TabActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tab);
+        SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
+        ViewPager viewPager = findViewById(R.id.view_pager);
+        viewPager.setAdapter(sectionsPagerAdapter);
+        TabLayout tabs = findViewById(R.id.tabs);
+        TabLayout.Tab firstTab = tabs.newTab(); // Create a new Tab names
+        firstTab.setText("First Tab"); // set the Text for the first Tab
+        //firstTab.setIcon(R.drawable.ic_launcher); // set an icon for the first tab
+        tabs.addTab(firstTab);
+        tabs.setupWithViewPager(viewPager);
+        FloatingActionButton fab = findViewById(R.id.fab);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+}
